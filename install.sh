ccefc4be9cca119aeec1ee24c1cdd8673f667698
Edoardo Costantini <edoardo.costantini@prima.it>
2023-07-24 12:08:26 +0200

    ignore

--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@ cp ./.zshrc $HOME/.zshrc
 cp i3/config $HOME/.config/i3/config
 cp alacritty/alacritty.yml $HOME/.config/alacritty/alacritty.yml
 mkdir -p $HOME/.config/polybar && cp -r polybar/* $HOME/.config/polybar/
+sudo cp polybar/pulseaudio-control.bash /usr/bin/pulseaudio-control
