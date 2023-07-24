031f6d0a17f33998fcf72bfdd1a10d3c5c649346
Edoardo Costantini <edoardo.costantini@prima.it>
2023-07-24 11:20:38 +0200

    update install

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 cp ./.zshrc $HOME/.zshrc
 cp i3/config $HOME/.config/i3/config
 cp alacritty/alacritty.yml $HOME/.config/alacritty/alacritty.yml
-mkdir -p $HOME/.config/polybar && cp polybar/config.ini $HOME/.config/polybar/config.ini
+mkdir -p $HOME/.config/polybar && cp -r polybar/* $HOME/.config/polybar/
