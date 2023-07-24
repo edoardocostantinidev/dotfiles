9ac3b686f1f7ffdf31a2e05ca61fff8843234df6
Edoardo Costantini <edoardo.costantini@prima.it>
2023-07-24 16:31:24 +0200

    add nvim

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ cp i3/config $HOME/.config/i3/config
 cp alacritty/alacritty.yml $HOME/.config/alacritty/alacritty.yml
 mkdir -p $HOME/.config/polybar && cp -r polybar/* $HOME/.config/polybar/
 sudo cp polybar/pulseaudio-control.bash /usr/bin/pulseaudio-control
+mkdir -p $HOME/.config/nvim/ && cp -r nvim/* $HOME/.config/nvim/
